5f1a7abc041ad7e015e45da634686ad5fd45fbf7
Marcus Vinicius <69313671+ViniWhiskyzito@users.noreply.github.com>
2024-11-07 15:08:06 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function multiplicar(a, b) {
+    return a * b;
+}
+function dizOi(nome) {
+    return "Eaew " + nome;
+}
+console.log(multiplicar(5, 3));
+console.log(dizOi("Bruno"));
---
+++ main.ts
@@ -0,0 +1,13 @@
+
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+
+function dizOi(nome: string): string {
+    return "Oi" + nome;
+}
+
+
+console.log(multiplicar(5, 3));  
+console.log(dizOi("Bruno"));    
